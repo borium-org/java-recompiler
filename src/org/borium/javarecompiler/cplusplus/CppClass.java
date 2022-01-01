07d247cb5d7792cff65fcf785930f88f45b6e0c4
Borium <borium.org@gmail.com>
2022-01-01 15:03:04 -0600

    Namespace processing.

--- src/org/borium/javarecompiler/cplusplus/CppClass.java
+++ src/org/borium/javarecompiler/cplusplus/CppClass.java
@@ -1,9 +1,14 @@
 package org.borium.javarecompiler.cplusplus;
 
+import java.util.*;
+
 import org.borium.javarecompiler.classfile.*;
 
 public class CppClass
 {
+	/** Java class file. */
+	private ClassFile classFile;
+
 	/** Simple class name. */
 	@SuppressWarnings("unused")
 	private String className;
@@ -14,15 +19,23 @@ public class CppClass
 
 	private CppField[] fields;
 
+	/** Full set of namespaces for all classes referenced from this class. */
+	private TreeSet<String> namespaces = new TreeSet<>();
+
+	/** Classes whose simple names are referenced from multiple namespaces. */
+	private TreeSet<String> multipleClasses = new TreeSet<>();
+
 	/**
-	 * Create the C++ class file given the Java class vile.
+	 * Create the C++ class file given the Java class file.
 	 *
 	 * @param classFile Java class file.
 	 */
 	public CppClass(ClassFile classFile)
 	{
-		extractCppClassName(classFile);
-		extractFields(classFile);
+		this.classFile = classFile;
+		extractCppClassName();
+		extractNamespaces();
+		extractFields();
 		// TODO Auto-generated constructor stub
 	}
 
@@ -42,7 +55,7 @@ public class CppClass
 	 *
 	 * @param classFile Class file to parse.
 	 */
-	private void extractCppClassName(ClassFile classFile)
+	private void extractCppClassName()
 	{
 		// TODO Auto-generated method stub
 		String[] split = classFile.getClassName().split("[.]");
@@ -56,7 +69,7 @@ public class CppClass
 		namespace = cppName.substring(0, pos - 1);
 	}
 
-	private void extractFields(ClassFile classFile)
+	private void extractFields()
 	{
 		ClassField[] javaFields = classFile.getFields();
 		fields = new CppField[javaFields.length];
@@ -66,5 +79,33 @@ public class CppClass
 			CppField field = new CppField(javaField);
 			fields[i] = field;
 		}
+	}
+
+	/**
+	 * Extract namespaces for each referenced class. All namespaces are recorded,
+	 * including namespaces for all used core Java packages. At same time check if
+	 * any simple-named class is referenced from multiple packages, and flag simple
+	 * class names as such.
+	 */
+	private void extractNamespaces()
+	{
+		HashMap<String, String> classes = new HashMap<>();
+		List<String> referencedClassNames = classFile.getReferencedClasses();
+		for (String referencedClassName : referencedClassNames)
+		{
+			int pos = referencedClassName.lastIndexOf('.');
+			String packageName = referencedClassName.substring(0, pos);
+			String[] split = packageName.split("[.]");
+			String namespace = String.join("::", split);
+			namespaces.add(namespace);
+
+			String className = referencedClassName.substring(pos + 1);
+			if (classes.containsKey(className) && !classes.containsValue(referencedClassName))
+			{
+				multipleClasses.add(className);
+			}
+			classes.put(className, referencedClassName);
+		}
+		namespaces.remove(namespace);
 	}
 }
