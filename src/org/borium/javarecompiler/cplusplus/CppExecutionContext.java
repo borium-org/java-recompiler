44112c747e026a8e30e2cdc71a291b9901b002f6
Borium <borium.org@gmail.com>
2022-03-13 13:32:44 -0500

    TABLESWITCH implementation.

--- src/org/borium/javarecompiler/cplusplus/CppExecutionContext.java
+++ src/org/borium/javarecompiler/cplusplus/CppExecutionContext.java
@@ -1790,7 +1790,19 @@ public class CppExecutionContext extends ExecutionContext implements ClassTypeSi
 
 	private void generateTABLESWITCH(IndentedOutputStream source, InstructionTABLESWITCH instruction)
 	{
-		notSupported(instruction);
+		String[] topOfStack = stack.pop().split(SplitStackEntrySeparator);
+		Assert(topOfStack[0].equals("int"), "TABLESWITCH: Integer expected");
+		source.iprintln("switch (" + topOfStack[1] + ")");
+		source.iprintln("{");
+		int value = instruction.getFirstValue();
+		for (int i = 0; i < instruction.getCaseCount(); i++)
+		{
+			source.iprintln("case " + (value + i) + ":");
+			source.iprintln("\tgoto " + instruction.getLabel(i) + ";");
+		}
+		source.iprintln("default:");
+		source.iprintln("\tgoto " + instruction.getDefaultLabel() + ";");
+		source.iprintln("}");
 	}
 
 	private void generateWIDE(IndentedOutputStream source, InstructionWIDE instruction)
